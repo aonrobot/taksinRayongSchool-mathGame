1e337a75e74dfbc242c556117494f44df9682641
Auttawut Wiriyakreng <aonrobotz@gmail.com>
2018-09-16 23:52:27 +0700

    change port to env

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 var Promise = require('promise');
 
-const PORT = 80;
+const PORT = process.env.PORT || 3000;
 const HOST = 'localhost';
 
 const db = mysql.createConnection({
